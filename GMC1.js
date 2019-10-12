01409e8c3559fe5def3d275acfeb0ba07ed8aa2f
abby2212 <52089035+abby2212@users.noreply.github.com>
2019-10-12 14:59:54 +0100

    Add files via upload

---
+++ GMC1.js
@@ -0,0 +1,23 @@
+const pets = [
+    { name: "Max", type: "dog", bornOn: 2018 },
+    { name: "Angel", type: "cat", bornOn: 2015 },
+    { name: "Jasper", type: "dog", bornOn: 2016 }
+  ];
+
+
+const getAge=(pet)=>new Date().getFullYear() - pet.bornOn;
+
+  
+const petsWithAge=pets.forEach((pet)=> {
+pet.age=getAge(pet)
+return pet
+})
+    console.log(petsWithAge);
+ 
+
+const dogs=pets.filter((pet)=> {return pet.type ==="dog"});
+console.log(dogs);
+
+
+let jasper=pets.find((pet)=>{return pet.name === "Jasper"}) ;
+console.log("Jasper is " + jasper.age + " years old");
