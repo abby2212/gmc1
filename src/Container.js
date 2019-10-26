99368a1eac2f0719bb8d7cf02695f27da0adf347
abby2212 <52089035+abby2212@users.noreply.github.com>
2019-10-26 16:19:59 +0100

    twoowt

---
+++ src/Container.js
@@ -0,0 +1,8 @@
+import React from "react";
+import List from "./List"
+function Container (){
+    return( <>
+    <List/>
+    </>)
+}
+export default Container ;
---
+++ src/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ListItem from "./ListItem"
+function List() {
+    return (
+        <>
+            <ListItem />
+            <ListItem />
+            <ListItem />
+        </>
+    )
+}
+
+export default List;
